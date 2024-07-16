145674925810dc5c0426ee15a764fbb2eb9072c4
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-16 16:09:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -19,6 +19,8 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  depends_on = [azurerm_virtual_network.vnet]
 }
 
 resource "azurerm_public_ip" "public_ip" {
@@ -40,6 +42,8 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+
+  depends_on = [azurerm_subnet.subnet]
 }
 
 resource "azurerm_virtual_machine" "vm" {
@@ -124,6 +128,7 @@ resource "azurerm_virtual_machine" "vm" {
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
 
 
 
