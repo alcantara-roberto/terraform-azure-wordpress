4d3ec59f6a74a5b54f1b1821afba48fe2eeb2214
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-17 11:13:38 -0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,19 @@
 variable "resource_group_name" {
+  type    = string
   default = "rg-terraform-wordpress"
 }
 
 variable "location" {
-  default = "westeurope"
+  type    = string
+  default = "West Europe"
 }
 
 variable "admin_username" {
+  type    = string
   default = "adminuser"
 }
 
 variable "admin_password" {
-  default = "AdminPass1234!"
+  type    = string
+  default = "Password123!"
 }
