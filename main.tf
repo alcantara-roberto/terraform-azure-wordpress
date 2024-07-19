8804f508f1d04aede24d3ba840710a5a7a203422
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-19 11:30:48 -0300

    Update main.tf
    NSG aplicado diretamente na subnet

--- main.tf
+++ main.tf
@@ -20,6 +20,9 @@ resource "azurerm_subnet" "subnet" {
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.1.0/24"]
+
+  # Apply the NSG to the subnet if needed
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_public_ip" "public_ip" {
@@ -58,8 +61,6 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
-
-  network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
 resource "azurerm_virtual_machine" "vm" {
