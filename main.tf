69f3175431b78bf82c0430be6d8f27ec4dcba46b
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-19 11:00:21 -0300

    Update main.tf
    provisionamento correto

--- main.tf
+++ main.tf
@@ -111,6 +111,7 @@ resource "null_resource" "provision" {
       "sudo usermod -aG docker ${var.admin_username}",
       "sudo curl -L https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose",
       "sudo chmod +x /usr/local/bin/docker-compose",
+      "sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose",
       "docker-compose -f /home/${var.admin_username}/docker-compose.yml up -d"
     ]
 
