32d646edce6ee79a75cd126acc8dd5cf0aacb4a0
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-20 09:22:19 -0300

    Update cloud-init.sh
    modificação de autenticação

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-# Atualizar o sistema e instalar Docker
-sudo apt-get update
-sudo apt-get install -y docker.io
+# Atualizar lista de pacotes no sistema
+apt-get update
 
-# Iniciar e habilitar o Docker
-sudo systemctl start docker
-sudo systemctl enable docker
+# Instalar o Docker
+apt-get install -y docker.io
+
+# Inicializar serviços Docker
+systemctl start docker
+systemctl enable docker
 
 # Adicionar o usuário ao grupo Docker
-sudo usermod -aG docker ${ADMIN_USERNAME}
+usermod -aG docker adminuser
 
 # Instalar Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
 
 # Criar o arquivo docker-compose.yml
-cat << EOF > /home/${ADMIN_USERNAME}/docker-compose.yml
+cat <<EOF > /home/adminuser/docker-compose.yml
 version: '3.1'
 
 services:
+
   wordpress:
     image: wordpress
     restart: always
@@ -27,19 +30,18 @@ services:
       - 80:80
     environment:
       WORDPRESS_DB_HOST: db
-      WORDPRESS_DB_USER: projetouser
-      WORDPRESS_DB_PASSWORD: projetopass
-      WORDPRESS_DB_NAME: projetodb
+      WORDPRESS_DB_USER: root
+      WORDPRESS_DB_PASSWORD: GAud4mZby8F3SD6P
+      WORDPRESS_DB_NAME: wordpress
 
   db:
     image: mysql:5.7
     restart: always
     environment:
-      MYSQL_DATABASE: projetodb
-      MYSQL_USER: projetouser
-      MYSQL_PASSWORD: projetopass
       MYSQL_ROOT_PASSWORD: GAud4mZby8F3SD6P
+      MYSQL_DATABASE: wordpress
 EOF
 
 # Subir os containers
-sudo docker-compose -f /home/${ADMIN_USERNAME}/docker-compose.yml up -d
+sudo -H -u adminuser bash -c 'docker-compose -f /home/adminuser/docker-compose.yml up -d'
+
