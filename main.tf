75aa28b4c5f55e3b8be7047d7b035816cc0f503f
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-21 19:25:04 -0300

    Update main.tf
    provisioner null resource

--- main.tf
+++ main.tf
@@ -1,5 +1,13 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.85.0"
+    }
+  }
+}
+
 provider "azurerm" {
-  version = "=3.85.0"
   features {}
 }
 
@@ -75,6 +83,8 @@ resource "azurerm_network_interface" "nic" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
+
+  depends_on = [azurerm_public_ip.public_ip]
 }
 
 resource "azurerm_virtual_machine" "vm" {
@@ -114,6 +124,22 @@ resource "azurerm_virtual_machine" "vm" {
   }
 }
 
+resource "null_resource" "wait_for_ip" {
+  provisioner "local-exec" {
+    command = "sleep 60"
+  }
+
+  depends_on = [azurerm_virtual_machine.vm]
+}
+
 output "public_ip_address" {
   value = azurerm_public_ip.public_ip.ip_address
+}
+
+resource "null_resource" "refresh_ip_output" {
+  provisioner "local-exec" {
+    command = "terraform apply -target=azurerm_public_ip.public_ip -auto-approve"
+  }
+
+  depends_on = [null_resource.wait_for_ip]
 }
