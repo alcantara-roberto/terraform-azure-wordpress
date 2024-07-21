41ce0d2b771b92a3ed53a057d6ee101538db00f0
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-21 20:36:13 -0300

    Update cloud-init.sh
    configuração docker

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,45 +1,42 @@
-#!/bin/bash
+resource "azurerm_virtual_machine" "vm" {
+  name                  = "vm"
+  location              = azurerm_resource_group.rg.location
+  resource_group_name   = azurerm_resource_group.rg.name
+  network_interface_ids = [azurerm_network_interface.nic.id]
+  vm_size               = "Standard_B1s"
 
-# Atualizar lista de pacotes no sistema
-sudo apt-get update
+  storage_os_disk {
+    name              = "osdisk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
 
-# Instalar o Docker
-sudo apt-get install -y docker.io
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
 
-# Inicializar serviços Docker
-sudo systemctl start docker
-sudo systemctl enable docker
+  os_profile {
+    computer_name  = "hostname"
+    admin_username = var.admin_username
+    admin_password = var.admin_password
+    custom_data    = filebase64("cloud-init.sh")
+  }
 
-# Adicionar o usuário ao grupo Docker
-sudo usermod -aG docker ${var.admin_username}
+  os_profile_linux_config {
+    disable_password_authentication = false
+  }
 
-# Instalar Docker Compose
-sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+  tags = {
+    environment = "TerraformDemo"
+  }
 
-# Criar o arquivo docker-compose.yml
-cat <<EOF > /home/${var.admin_username}/docker-compose.yml
-version: '3.1'
+  depends_on = [
+    azurerm_network_interface.nic,
+    azurerm_public_ip.public_ip
+  ]
+}
 
-services:
-  wordpress:
-    image: wordpress
-    restart: always
-    ports:
-      - 80:80
-    environment:
-      WORDPRESS_DB_HOST: db
-      WORDPRESS_DB_USER: root
-      WORDPRESS_DB_PASSWORD: GAud4mZby8F3SD6P
-      WORDPRESS_DB_NAME: wordpress
-
-  db:
-    image: mysql:5.7
-    restart: always
-    environment:
-      MYSQL_ROOT_PASSWORD: GAud4mZby8F3SD6P
-      MYSQL_DATABASE: wordpress
-EOF
-
-# Subir os containers
-sudo -H -u ${var.admin_username} bash -c 'docker-compose -f /home/${var.admin_username}/docker-compose.yml up -d'
