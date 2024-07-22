9d7df2f9a648a256b7096362165415f0440fc6a2
alcantara-roberto <161370909+alcantara-roberto@users.noreply.github.com>
2024-07-22 13:19:06 -0300

    Update variables.tf
    adicionado campo observação sobre a senha

--- variables.tf
+++ variables.tf
@@ -10,6 +10,7 @@ variable "admin_username" {
   default = "adminuser"
 }
 
+# senha passa a se tornar "GAud4mZby8F3SD6P" após a inicialização
 variable "admin_password" {
   default = "AdminPass1234!"
 }
